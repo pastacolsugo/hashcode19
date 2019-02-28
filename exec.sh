c9bfe6e204478abd2e69078e34d4fdf2544267ed
Ugo Baroncini <ugobaroncini@gmail.com>
2019-02-28 22:55:14 +0100

    bash script for testing

---
+++ exec.sh
@@ -0,0 +1,10 @@
+./input < a_example.txt > a_out.txt
+
+./input < b_lovely_landscapes.txt > b_out.txt
+
+./input < c_memorable_moments.txt > c_out.txt
+
+./input < d_pet_pictures.txt > d_out.txt
+
+./input < e_shiny_selfies.txt > e_out.txt
+
