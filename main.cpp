d9ae00b901552e2a59638a3e1775627afa929c62
Samuele Ceroni <samuele.ceroni@gmail.com>
2019-02-28 20:08:39 +0100

    ok

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <string>
+#include <queue>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+struct Image {
+    int index;
+    bool isVertical;
+    vector <string> tags;
+};
+
+struct Node {
+    string name; // "0", "1", "2 3", "4"
+    vector<pair<int, int> > edges;
+}
+
+int main(){
+    return 0;
+}
